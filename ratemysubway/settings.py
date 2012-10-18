6704e8a0fc80b096d9c4b8100953d133fc04570d
Rob Tuck <robtuck@gmail.com>
2012-10-18 16:55:31 -0400

    adding google

--- ratemysubway/settings.py
+++ ratemysubway/settings.py
@@ -134,6 +134,7 @@ INSTALLED_APPS = (
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
 )
 
 AUTHENTICATION_BACKENDS = (
